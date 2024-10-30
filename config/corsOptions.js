54493698cd1f11861986d43fa01b65fec045eaa6
Faiz Kirsten <faizkirsten@gmail.com>
2024-10-30 18:41:46 +0200

    Initial Commit

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     "https://user-credential-manager.netlify.app/",
     "http://127.0.0.1:5500",
     "http://localhost:3000",
+    "https://user-credential-manager-backend.vercel.app/",
 ];
 
 // remove !origin before deployment
